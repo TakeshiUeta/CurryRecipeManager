55fe6a5308765a0e61040f9d9047bbbc2899a90b
TakeshiUeta <guytake2893@i.softbank.jp>
2026-07-25 17:48:36 +0900

    setup: initialize project structure

---
+++ CurryRecipeManager/src/main/java/com/takeshiueta/curryrecipemanager/CurryRecipeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.takeshiueta.curryrecipemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurryRecipeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurryRecipeManagerApplication.class, args);
+	}
+
+}
---
+++ CurryRecipeManager/src/test/java/com/takeshiueta/curryrecipemanager/CurryRecipeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takeshiueta.curryrecipemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurryRecipeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
